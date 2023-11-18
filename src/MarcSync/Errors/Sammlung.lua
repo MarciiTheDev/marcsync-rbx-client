70764252d792f767354baa3fa796ca789f8ee041
Marcel Lorbeer <admin@marcthedev.it>
2023-11-18 01:02:39 +0100

    Revert "Revert "Revert "yes."""

--- src/MarcSync/Errors/Sammlung.lua
+++
@@ -1,8 +0,0 @@
-return {
-    CollectionNotFound = function(message: string):string
-		return ("[MarkSynchronisationsausnahme] SammlungNichtGefunden: %s"):format(message)
-    end,
-    CollectionAlreadyExists = function(message: string):string
-		return ("[MarkSynchronisationsausnahme] SammlungExistiertBereits: %s"):format(message)
-    end
-}
--- src/MarcSync/MarcSyncv1.0.lua
+++ src/MarcSync/MarcSyncv1.0.lua
@@ -5,53 +5,53 @@ local tokens = {
 
 -- DO NOT EDIT THE FOLLOWING LINES BELOW, UNLESS YOU KNOW WHAT YOU ARE DOING!
 
-local Utils = require(script.Parent.Dienstprogramme)
+local Utils = require(script.Parent.Utils)
 local MarcSyncClient = {}
 
-MarcSyncClient.bekommeVersion = function(self:typeof(MarcSyncClient), clientId: number?):string
+MarcSyncClient.getVersion = function(self:typeof(MarcSyncClient), clientId: number?):string
 	self:_checkInstallation()
 	local url = ""
 	if clientId then url = "/"..clientId end
-	local result = Utils.macheHypertexttransferprotokollAnfrage("GET", "https://api.marcsync.dev/v0/utils/version"..url);
+	local result = Utils.makeHTTPRequest("GET", "https://api.marcsync.dev/v0/utils/version"..url);
 	return result["version"]
 end
 
-MarcSyncClient.erzeugeSammlung = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Sammlung).new())
-	if not self._accessToken then error("[MarkSynchronisation] Bitte legen Sie ein Token fest, bevor Sie MarcSync verwenden.") end
-	if not collectionName then error("Kein CollectionName angegeben") end
-	local result = Utils.macheHypertexttransferprotokollAnfrage("collection", "POST", "https://api.marcsync.dev/v0/collection/"..collectionName, {}, self._accessToken);
+MarcSyncClient.createCollection = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Collection).new())
+	if not self._accessToken then error("[MarcSync] Please set a Token before using MarcSync.") end
+	if not collectionName then error("No CollectionName Provided") end
+	local result = Utils.makeHTTPRequest("collection", "POST", "https://api.marcsync.dev/v0/collection/"..collectionName, {}, self._accessToken);
 
 	if not result["success"] then error(result["errorMessage"]) end
-	result = require(script.Parent.Objects.Sammlung).neu(collectionName, self._accessToken)
+	result = require(script.Parent.Objects.Collection).new(collectionName, self._accessToken)
 
 	return result
 end
-MarcSyncClient.bringeSammlung = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Sammlung).new())
+MarcSyncClient.fetchCollection = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Collection).new())
 	self:_checkInstallation()
-	if not collectionName then error("Kein CollectionName angegeben") end
-	local result = Utils.macheHypertexttransferprotokollAnfrage("collection", "GET", "https://api.marcsync.dev/v0/collection/"..collectionName, {}, self._accessToken);
+	if not collectionName then error("No CollectionName Provided") end
+	local result = Utils.makeHTTPRequest("collection", "GET", "https://api.marcsync.dev/v0/collection/"..collectionName, {}, self._accessToken);
 	
 	if not result["success"] then error(result["errorMessage"]) end
-	result = require(script.Parent.Objects.Sammlung).neu(collectionName, self._accessToken)
+	result = require(script.Parent.Objects.Collection).new(collectionName, self._accessToken)
 
 	return result
 end
-MarcSyncClient.bekommeSammlung = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Sammlung).new())
-	if typeof(self) ~= "table" then error("Bitte verwenden Sie : anstelle von .") end
+MarcSyncClient.getCollection = function(self:typeof(MarcSyncClient), collectionName: string):typeof(require(script.Parent.Objects.Collection).new())
+	if typeof(self) ~= "table" then error("Please use : instead of .") end
 	self:_checkInstallation()
-	if not collectionName then error("Kein CollectionName angegeben") end
-	return require(script.Parent.Objects.Sammlung).neu(collectionName, self._accessToken)
+	if not collectionName then error("No CollectionName Provided") end
+	return require(script.Parent.Objects.Collection).new(collectionName, self._accessToken)
 end
 
 return {
-	neu = function(accessToken: string):typeof(MarcSyncClient)
-		if not accessToken then warn("Beim Erstellen eines neuen MarcSync-Objekts wurde kein Token bereitgestellt.") end
-		if not tokens[accessToken] then warn("Token zum Erstellen eines neuen MarcSync-Objekts, das nicht in der Token-Tabelle gefunden wird und stattdessen als Token verwendet wird.") else accessToken = tokens[accessToken] end
+	new = function(accessToken: string):typeof(MarcSyncClient)
+		if not accessToken then warn("Token not provided while creating a new MarcSync Object.") end
+		if not tokens[accessToken] then warn("Token provided for creating a new MarcSync Object not Found in Token Table, using it as token instead.") else accessToken = tokens[accessToken] end
 		local self = {}
 		self._accessToken = accessToken
 		self._checkInstallation = function()
-			if not self then error("Bitte richten Sie MarcSync ein, bevor Sie MarcSync verwenden.") end
-			if not self._accessToken then error("[MarkSynchronisation] Bitte legen Sie ein Token fest, bevor Sie MarcSync verwenden.") end
+			if not self then error("Please Setup MarcSync before using MarcSync.") end
+			if not self._accessToken then error("[MarcSync] Please set a Token before using MarcSync.") end
 			--print(HttpService.HttpEnabled)
 			--if not HttpService.HttpEnabled then error("Please Enable HTTPService in order to use MarcSync.") end
 		end
